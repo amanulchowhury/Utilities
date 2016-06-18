b227923f960ab33f0d215e7e9e596b2431e6e64a
Amanul Chowdhury <amanulchowhury@users.noreply.github.com>
2016-06-18 02:23:50 -0700

    Create recursiveArraySum.js

---
+++ recursiveArraySum.js
@@ -0,0 +1,25 @@
+// input [ 1, 2, 3, [3, 4], 'a', 'b', 1 ]
+
+
+function recursiveArraySum(arr) {
+	let sum = 0;
+	if(Math.round(arr) === arr) {
+		return arr;
+	}
+	
+	if(!Array.isArray(arr)) { 
+		throw Error('should be a number or an array');
+	}
+
+	return arr.reduce( (prev, current) => { 
+		if(Array.isArray(current)) {
+			prev += arrSum(current);
+		}
+
+		if(Math.round(current) === current) {
+			prev += current; 
+		} 
+
+		return prev;
+	}, 0);
+} 
