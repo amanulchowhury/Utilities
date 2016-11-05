eaae1616b939121e1e5ad9d1fb55c4be87835abf
Amanul Chowdhury <amanulchowdhury@Amanuls-MacBook-Pro.local>
2016-11-04 20:11:04 -0700

    converted menuBuilder to ES6

--- MenuBuilder.js
+++ MenuBuilder.js
@@ -36,69 +36,14 @@
 // ***********************************
 
 /**
- * MenuBuilder constructor
- * @constructor
- */
-MenuBuilder = function () {
-	this.tree = {
-		name: 'root',
-		id: null,
-		children: []
-	};
-};
-
-/**
- * Compares two given nodes based on ordinal.
- * @param firstObj First node
- * @param secondObj Second node.
- * @returns {number} Difference between ordinals
- */
-MenuBuilder.prototype.compareNodes = function (firstObj, secondObj) {
-	return firstObj.ordinal - secondObj.ordinal;
-};
-
-/**
- * Parses the data to build a tree.
- * @param data
- * @returns {*}
- */
-MenuBuilder.prototype.parseTree = function (data) {
-	return this.buildTree.call(this, data, this.tree);
-};
-
-MenuBuilder.prototype.buildTree = function (data, tree) {
-	var i = 0;
-	if (Object.prototype.toString.call(data) === '[object Array]' && !data.length) {
-		return;
-	}
-
-	if (!tree.children) {
-		tree.children = [];
-	}
-
-	while (i < data.length) {
-		if (data[i].parentId === tree._id) {
-			tree.children.push(this.buildTree(data, data[i]));
-		}
-
-		i++;
-	}
-	if (tree.children.length) {
-		tree.children.sort(this.compareNodes);
-	}
-
-	return tree;
-};
-
-/**
- * Prints the tree.
+ * Prints the root to nav menu
  * @param tree
  * @returns {string}
  */
-MenuBuilder.prototype.printTree = function (tree) {
+const printTree = (tree) => {
 	var ul = '<ul>';
 	for (var i = 0; i < tree.children.length; i++) {
-		ul += '<li>' + tree.children[i].name + this.printTree.call(this, tree.children[i]);
+		ul += '<li>' + tree.children[i].name + printTree(tree.children[i]);
 		ul += '</li>';
 	}
 
@@ -106,9 +51,75 @@ MenuBuilder.prototype.printTree = function (tree) {
 	return ul;
 };
 
-module.exports = MenuBuilder;
+/**
+ * Class to represent a root node.
+ */
+class Node {
+	constructor() {
+		this.name = '';
+		this.id = null;
+		this.children = [];
+		this.ordinal = 0;
+	}
+}
+
+/**
+ * Menubuilder Class
+ */
+class Tree {
+	constructor(data) {
+		this.root = new Node();
+		this.buildTree(data, this.root);
+	}
+
+	/**
+	 * Compare two nodes based on the ordinal value
+	 * @param firstObj
+	 * @param secondObj
+	 * @returns {number}
+	 */
+	compareNodes(firstObj, secondObj) {
+		return firstObj.ordinal - secondObj.ordinal;
+	}
+
+	/**
+	 * Build root structure
+	 * @param data tree data
+	 * @param root root of the tree
+	 */
+	buildTree(data, root) {
+		var i = 0;
+		if (Array.isArray(data) && !data.length) {
+			return;
+		}
+
+		if (!root.children) {
+			root.children = [];
+		}
+
+		while (i < data.length) {
+			if (data[i].parentId === root._id) {
+				root.children.push(this.buildTree(data, data[i]));
+			}
+
+			i++;
+		}
+		if (root.children.length) {
+			root.children.sort(this.compareNodes);
+		}
+	}
+
+	/**
+	 * Prints the tree
+	 * @returns {string}
+	 */
+	printTree() {
+		return printTree(this.root);
+	}
+}
+
+module.exports = Tree;
 
 /*********** Usage ***********/
-// var builder = new MenuBuilder();
-// var tree = builder.parseTree(data);
-// document.write(builder.printTree(tree));
+// var builder = new Tree(data);
+// document.write(builder.printTree());
